4883b002d5bbca168aeb097d14632d2a16d5873e
Ruslan <arhara81@gmail.com>
2023-05-02 19:49:32 +0300

    added prizes

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,91 @@
 package src;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     private static LinkedList<ToyShop> toys = new LinkedList<>();
 
     public static void main(String[] args) {
+
         ToyShop toyShop = new ToyShop("Base");
-        toyShop.addToyToShop(1, "Мишка",1,1);
-        toyShop.addToyToShop(2, "Собачка",1,1);
-        toyShop.addToyToShop(3, "Заяц",1,1);
-        toyShop.addToyToShop(4, "Чебурашка",1,1);
+        toyShop.addToyToShop(1, "Мишка",1,5);
+        toyShop.addToyToShop(2, "Собачка",1,10);
+        toyShop.addToyToShop(3, "Заяц",1,15);
+        toyShop.addToyToShop(4, "Чебурашка",1,20);
+        toyShop.addToyToShop(5, "Погремушка",1,25);
         toys.add(toyShop);
 
-        toyShop = new ToyShop("All toys");
-
-        toyShop.addToyToShop(1, "Чебурашка",1,1);
-        toyShop.addToyToShop(2, "Неваляшка",1,1);
-        toyShop.addToyToShop(3, "Погремушка",1,1);
-        toyShop.addToyToShop(4, "Чебурашка",1,1);
-
-        toys.add(toyShop);
+//        toyShop = new ToyShop("All toys");
+//
+//        toyShop.addToyToShop(1, "Чебурашка",1,1);
+//        toyShop.addToyToShop(2, "Неваляшка",1,1);
+//        toyShop.addToyToShop(3, "Погремушка",1,1);
+//        toyShop.addToyToShop(4, "Чебурашка",1,1);
+//
+//        toys.add(toyShop);
 
         LinkedList<Toy>toyList1 = new LinkedList<>();
 
         toys.get(0).addToLotteryList("Чебурашка", toyList1);
-        toys.get(1).addToLotteryList("Погремушка", toyList1);
+        toys.get(0).addToLotteryList("Погремушка", toyList1);
         toys.get(0).addToLotteryList("Заяц", toyList1);
+        //toyList1.remove(1);
         //List<Toy> toysInList
-        //System.out.println(toys.toString());
+        //System.out.println(toyList1);
+        // toyShop.showToyShopList(toys);
+        //toyShop.showToyLotteryList(toyList1);
+        //printMenu();
+        start(toyList1);
 
-//        LinkedList<Toy>toyList_1 = new LinkedList<>();
-//        toys.get(0).addToyToShop("Мишка",toyList_1);
-//        toys.get(0).addToyToShop("Собачка",toyList_1);
-//        toys.get(1).addToLotteryList("Чебурашка",toyList_1);
-//        toys.get(1).addToLotteryList("Неваляшка",toyList_1);
-//        toys.get(1).addToLotteryList("Погремушка",toyList_1);
-        //System.out.println(toys.get(0).toString());
-        toyShop.showToyShopList(toys);
-        toyShop.showToyLotteryList(toyList1);
+        //
+        // new ToyShop("Lottery").addToLotteryList("Заяц", toyList1 );
+
     }
 
-//    public static void showToyShopList(LinkedList<Toy>toyList){
-//        Iterator<Toy> iterator = toyList.iterator();
-//        System.out.println("____");
-//        while (iterator.hasNext()){
-//            System.out.println(iterator.next());
-//        }
-//        System.out.println("----");
-//    }
+    public static void start(LinkedList<Toy>toyList1) {
+        Scanner sc = new Scanner(System.in);
+        boolean quit = false;
+        boolean forward = true;
+        printMenu();
+        while(!quit){
+            int action = sc.nextInt();
+            sc.nextLine();
+            switch (action){
+                case 0:
+                    System.out.println("Досвидос");
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.println("Список магазина");
+                    ToyShop.showToyShopList(toys);
+                    break;
+                case 2:
+                    System.out.println("Список розыгрыша");
+                    ToyShop.showToyLotteryList(toyList1);
+                    break;
+                case 3:
+                    System.out.println("ВВедите назвние игрушки, которую хотите перместить в сапсок розыгрыша :");
+                    String name = sc.nextLine();
+                    //new ToyShop("Lottery").addToLotteryList(name,LotteryList );
+                    toys.get(0).addToLotteryList(name, toyList1);
+                    break;
+                case 4 :
+                    System.out.println("Sorry this function is not avaylable");
+                    break;
+                case 5:
+                    ToyShop.putToyToPrizeToysQueue();
+                    ToyShop.getPrizeToyAndWrite("prizes.txt" );
+            }
+        }
+
+    }
+    public static void printMenu(){
+        System.out.println("0 - Выход\n " +
+                "1 - Посмотреть список магазина\n " +
+                "2 - Посмотреть список на розыгрыш\n" +
+                "3 - Переместить из списка маганина в список розыгрыш\n"+
+                "4 - Удалить игрушку из списка розыгрыша\n" +
+                "5 - Провести розыгрыш\n" +
+                "6 - Забрать призовую игрушку");
+    }
+
 }
--- src/ToyShop.java
+++ src/ToyShop.java
@@ -1,24 +1,24 @@
 package src;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class ToyShop {
     private String name;
-    private ArrayList<Toy>toys;
+    private static ArrayList<Toy>toys;
+    private static Queue<Toy> prizeToyQueue = new LinkedList<>();
     public ToyShop(String name) {
         this.name = name;
         this.toys = new ArrayList<Toy>();
     }
 
-    public void addToyToShop(int id, String name, int quantity, int chance){
+    public static void addToyToShop(int id, String name, int quantity, int chance){
         toys.add(new Toy(id, name, quantity, chance));
         // System.out.println(name + "added");
     }
 
-    public void showToyLotteryList(LinkedList<Toy>toyList){
+    public static void showToyLotteryList(LinkedList<Toy>toyList){
         Iterator<Toy> iterator = toyList.iterator();
         System.out.println("____");
         while (iterator.hasNext()){
@@ -27,30 +27,63 @@ public class ToyShop {
         System.out.println("----");
     }
 
-    public void showToyShopList(LinkedList<ToyShop>toyList) {
-        for (int i = 0; i < toyList.size(); i++) {
-            Iterator<ToyShop> iterator = toyList.iterator();
-            System.out.println("____");
-            while (iterator.hasNext()) {
-                System.out.println(iterator.next().toString());
-            }
-            System.out.println("----");
-        }
+    public static void showToyShopList(LinkedList<ToyShop>toyList) {
+
+        System.out.println(toyList);
     }
 
     public void addToLotteryList(String name, LinkedList<Toy> LotteryList){
-        for (Toy checkedToy :this.toys) {
+        for (Toy checkedToy : this.toys) {
+            //System.out.println("YYYY");
             if(checkedToy.getName().equals(name)){
                 LotteryList.add(checkedToy);
+                System.out.println(checkedToy.name +" Добавлен в список");
             }
         }
     }
 
-//    @Override
-//    public String toString() {
-//        return "ToyShop name"
-//                 + name + '\n' +
-//                ", toys=" + toys +
-//                '}';
-//    }
+    public static void putToyToPrizeToysQueue() {
+        if (toys.isEmpty()) {
+            System.out.println("В магазине нет игрушек");
+            return;
+        }
+        ArrayList<Toy> toysByChanceList = new ArrayList<>();
+        for (Toy toy : toys) {
+            int currentToyChance = toy.getChance();
+            for (int i = 0; i < currentToyChance; i++) {
+                toysByChanceList.add(toy);
+            }
+        }
+        Collections.shuffle(toysByChanceList);
+        Toy toy = toysByChanceList.get(0);
+        prizeToyQueue.add(toy);
+        toys.remove(toy);
+    }
+
+    public static void getPrizeToyAndWrite(String filePath) {
+        if (prizeToyQueue.isEmpty()) {
+            System.out.println("Призовых игрушек в наличии нет");
+            return;
+        }
+        Toy prizeToy = prizeToyQueue.poll();
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            writer.write(prizeToy.getName() + "\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Не удалось записать призовую игрушку в файл");
+        }
+        System.out.println("Поздравляем! Вы выиграли " + prizeToy.getName());
+        toys.remove(prizeToy);
+    }
+
+//    public void remove
+
+    @Override
+    public String toString() {
+        return "ToyShop name"
+                + name + '\n' +
+                ", toys=" + toys +
+                '}';
+    }
 }
